5501bf912c4ae51fbd3ba6f69823064f011ac69e
Tomato150 <tomcat.frantz@gmail.com>
2017-10-17 16:10:30 +1100

    Accommodation for redux and implementation of game world element

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,44 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
-import AppReducer from './reducers/index'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {logger} from 'redux-logger';
 
-const PIXI = require('pixi.js');
-import ReactPIXI from 'react-pixi';
+import AppReducer from './reducers/index';
+import {trial} from './actions/index'
 
-const store = createStore(AppReducer);
+import GameMap from './gui/game_map';
 
+
+const middleware = applyMiddleware(logger);
+const store = createStore(AppReducer, middleware);
+
+// For note, server_data is a global variable that allows access to data passed by server.
+// See more within index.html
+
+@connect(store => {
+    return {worldState: store.World}
+})
 class App extends Component {
     render() {
+        // const {dispatch, worldState} = this.props;
+        const {world, world_metadata} = server_data;
         return (
-            <Provider store={store}>
-                <div>Hey, that's pretty good</div>
-            </Provider>
+            <div>
+                <GameMap
+                    data={world}
+                    metadata={world_metadata}
+                    width={world_metadata['TILE SIZE'] * world_metadata['WORLD WIDTH']}
+                    height={world_metadata['TILE SIZE'] * world_metadata['WORLD HEIGHT']}
+                />
+            </div>
         )
     }
 }
 
 ReactDOM.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('react-entry')
 );
